6d70d17a0b4506e291f10fb1e19b6e87b95f56e1
Kadet44 <kadet.dim@gmail.com>
2017-03-31 12:38:21 +0300

    окно вылетает  по центру

--- src/graphic_Interface/Warning.java
+++ src/graphic_Interface/Warning.java
@@ -7,7 +7,8 @@ import javax.swing.JFrame;
 
 public class Warning {
   static Point loc_frame;
-  	
+  private int x  = (int) loc_frame.getX();
+  private int y = (int) loc_frame.getY();
 
 
 
@@ -20,12 +21,14 @@ public static Point getLoc_frame() {
 
 public static void setLoc_frame(Point loc_frame) {
 	Warning.loc_frame = loc_frame;
+	
 }
 
 public Warning() {
 	  JFrame warning_frame = new JFrame("Внимание!!!");
-		warning_frame.setSize(300, 200);
-		warning_frame.setLocation(loc_frame);
+		warning_frame.setSize(300, 150);
+		warning_frame.setLocation(x+150, y+50);
+		//warning_frame.setLocation(loc_frame);
 		warning_frame.setVisible(true);
 }
 }
